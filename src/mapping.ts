6a84607dc59fea29c7f5c52d4de9e965fddbe070
ajs <palebluedot@gmail.com>
2020-04-18 22:05:45 -0300

    First commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,45 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+// Templates.
+import { Kernel as KernelTemplate } from '../generated/templates'
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+// Contracts.
+import { Kernel as KernelContract } from '../generated/templates/Kernel/Kernel'
+
+// Events.
+import { DeployDAO as DeployDAOEvent } from '../generated/DAOFactory/DAOFactory'
+import { SetApp as SetAppEvent } from '../generated/templates//Kernel/Kernel'
+
+// Entities.
+import {
+  DAO as DAOEntity,
+  App as AppEntity
+} from '../generated/schema'
+
+export function handleDeployDAO(event: DeployDAOEvent): void {
+  let daoAddress = event.params.dao
+  let daoId = daoAddress.toHex()
+
+  KernelTemplate.create(daoAddress)
+
+  let kernelContract = KernelContract.bind(daoAddress)
+
+  let daoEntity = new DAOEntity(daoId)
+  daoEntity.address = daoAddress
+  daoEntity.acl = kernelContract.acl()
+  daoEntity.save()
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleSetApp(event: SetAppEvent): void {
+  // SetApp events are emitted by Kernels (DAOs).
+  // An event's address property represents the contract that emitted the event.
+  // So, the event's address is the DAO that emitted the event.
+  let daoAddress = event.address
+  let daoId = daoAddress.toHex()
+
+  let appAddress = event.params.app
+  let appId = appAddress.toHex()
+
+  let appEntity = new AppEntity(appId)
+  appEntity.address = appAddress
+  appEntity.dao = daoId
+  appEntity.save()
 }
